c253eb288a104c35a0f520c04d4f5782177a5871
kalpha <162287131+gguatit@users.noreply.github.com>
2025-12-03 15:21:59 +0900

    Add files via upload

---
+++ 20_PEPE/index.js
@@ -0,0 +1,7 @@
+setInterval(()=>{
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a, i)=>{
+        a.src = 'https://tse4.mm.bing.net/th/id/OIP.4IJOrxinLh2oMCHkquIV7AHaHl?rs=1&pid=ImgDetMain&o=7&rm=3'
+    })
+}, 500)
