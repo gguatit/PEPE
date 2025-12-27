73c5183985f749935e6bc13384922e3d969ade45
kalpha <162287131+gguatit@users.noreply.github.com>
2025-12-27 02:48:10 +0000

    index.js에서 pepeImage 객체 추가 및 drawImage, createPattern 메서드 수정. manifest.json 파일 재생성.

--- 20_PEPE/index.js
+++ 20_PEPE/index.js
@@ -1,13 +1,15 @@
 const pepeUrl = 'https://tse4.mm.bing.net/th/id/OIP.4IJOrxinLh2oMCHkquIV7AHaHl?rs=1&pid=ImgDetMain&o=7&rm=3';
 
+const pepeImage = new Image();
+pepeImage.crossOrigin = 'anonymous';
+pepeImage.src = pepeUrl;
+
 setInterval(()=>{
-    // img 태그 변경
     let imgs = document.querySelectorAll('img');
     imgs.forEach((img)=>{
         img.src = pepeUrl;
     });
 
-    // 배경 이미지 변경
     let allElements = document.querySelectorAll('*');
     allElements.forEach((element)=>{
         let bgImage = window.getComputedStyle(element).backgroundImage;
@@ -16,10 +18,42 @@ setInterval(()=>{
         }
     });
 
-    // CSS 변수로 설정된 배경 이미지도 변경
     document.querySelectorAll('[style*="background"]').forEach((element)=>{
         if (element.style.background || element.style.backgroundImage) {
             element.style.backgroundImage = `url(${pepeUrl})`;
         }
     });
 }, 500);
+
+// Canvas 2D context: drawImage 가로채기
+(function(){
+    const proto = CanvasRenderingContext2D && CanvasRenderingContext2D.prototype;
+    if (!proto) return;
+
+    const origDrawImage = proto.drawImage;
+    proto.drawImage = function(...args){
+        try{
+            const src = args[0];
+            const isElement = (typeof HTMLImageElement !== 'undefined' && src instanceof HTMLImageElement)
+                            || (typeof HTMLVideoElement !== 'undefined' && src instanceof HTMLVideoElement)
+                            || (typeof HTMLCanvasElement !== 'undefined' && src instanceof HTMLCanvasElement)
+                            || (typeof ImageBitmap !== 'undefined' && src instanceof ImageBitmap);
+            if (isElement) args[0] = pepeImage;
+        }catch(e){}
+        return origDrawImage.apply(this, args);
+    };
+
+    const origCreatePattern = proto.createPattern;
+    if (origCreatePattern) {
+        proto.createPattern = function(image, repetition){
+            try{
+                const isElement = (typeof HTMLImageElement !== 'undefined' && image instanceof HTMLImageElement)
+                                || (typeof HTMLVideoElement !== 'undefined' && image instanceof HTMLVideoElement)
+                                || (typeof HTMLCanvasElement !== 'undefined' && image instanceof HTMLCanvasElement)
+                                || (typeof ImageBitmap !== 'undefined' && image instanceof ImageBitmap);
+                if (isElement) image = pepeImage;
+            }catch(e){}
+            return origCreatePattern.call(this, image, repetition);
+        };
+    }
+})();
